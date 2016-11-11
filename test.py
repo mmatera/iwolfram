0e8faf76fa08efc1cbfb5db42bd445eae8e40779
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2016-11-11 07:36:22 -0300

    Update test.py

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ class MyKernelTests(jupyter_kernel_test.KernelTests):
     # to the frontend
     # TODO
     # code_clear_output = "clear_output()"
-    def subTest(code):
+    def subTest(code=None,hist_access_type=None):
         return True
     
 if __name__ == '__main__':
