69815a5e9b36aae166406f0d1b18144d8f061e41
mauricio matera <matera@fisica.unlp.edu.ar>
2016-11-20 10:12:43 -0300

    some tests

--- nbmathics/static/main.js
+++ nbmathics/static/main.js
@@ -2,6 +2,20 @@
 define(
     function() {
 	function _load_ipython_extension(){
+	    var handlerCtrlEsc = function(){
+		alert("You press CtrlEsc");
+	    };
+	    var actionCtrlEsc = {
+		icon: 'fa-comment-o',
+		help: 'Show an alert',
+		help_index: 'zz',
+		handler: handlerCtrlEsc
+	    };
+	    var prefix = 'nbmathics';
+	    var action_name = 'open-close-symbol';
+	    var full_action_nameCtrlEsc = Jupyter.actions.register(actionCtrlEsc, action_nameCtrlEsc, prefix);
+	    Jupyter.toolbar.add_buttons_group([full_action_nameCtrlEsc]);
+	    Jupyter.keyboard_manager.command_shortcuts.add_shortcut('Ctrl-Esc','nbmathics:open-close-symbol');
 	    console.info('this is my nbextension');
 	};
 	return {load_ipython_extension: _load_ipython_extension};
