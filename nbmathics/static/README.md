8da754b1694cb0e33fcbb5f07055516064e55aa9
mauricio matera <matera@fisica.unlp.edu.ar>
2016-11-20 13:54:17 -0300

    improving nbmathics

---
+++ nbmathics/static/README.md
@@ -0,0 +1,5 @@
+
+
+NBMathics loads some tools to process graphics in the iwolfram kernel, as well as some features for
+input symbols in the mma way.
+
--- nbmathics/static/main.js
+++ nbmathics/static/main.js
@@ -2,6 +2,7 @@
 define(
     function() {
 	function _load_ipython_extension(){
+	    alert("init nbmathics");
 	    var handlerCtrlEsc = function(){
 		alert("You press CtrlEsc");
 	    };
@@ -12,11 +13,14 @@ define(
 		handler: handlerCtrlEsc
 	    };
 	    var prefix = 'nbmathics';
-	    var action_name = 'open-close-symbol';
+	    var action_nameCtrlEsc = 'open-close-symbol';
+	    console.info('nbmathics: Registering action');
 	    var full_action_nameCtrlEsc = Jupyter.actions.register(actionCtrlEsc, action_nameCtrlEsc, prefix);
+	    console.info('nbmathics: adding button to the toolbar');
 	    Jupyter.toolbar.add_buttons_group([full_action_nameCtrlEsc]);
-	    Jupyter.keyboard_manager.command_shortcuts.add_shortcut('Ctrl-Esc','nbmathics:open-close-symbol');
-	    console.info('this is my nbextension');
+	    console.info('nbmathics: adding keybinding');
+	    Jupyter.keyboard_manager.edit_shortcuts.add_shortcut('Ctrl-Esc','nbmathics:open-close-symbol');
+	    console.info('this is my nbextension... done');
 	};
 	return {load_ipython_extension: _load_ipython_extension};
     }
