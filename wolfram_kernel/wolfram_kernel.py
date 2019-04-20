c9f5b352322ffd3172eff82be2d8c2e61ce058c0
Juan Mauricio Matera <matera@fisica.unlp.edu.ar>
2019-04-20 18:04:25 -0300

    Update wolfram_kernel.py

--- wolfram_kernel/wolfram_kernel.py
+++ wolfram_kernel/wolfram_kernel.py
@@ -234,7 +234,7 @@ class WolframKernel(ProcessMetaKernel):
     def check_wolfram(self):
         #starttext = os.popen("bash -c 'echo |" +
         #                     self.language_info['exec'] + "'").read()
-	starttext = os.popen(self.language_info['exec']).read()
+        starttext = os.popen(self.language_info['exec']).read()
         if starttext[:11] == "Mathematica":
             self.kernel_type = "wolfram"
         elif starttext[:8] == "\nMathics":
